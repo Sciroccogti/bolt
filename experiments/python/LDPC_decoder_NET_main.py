ab4a7d26250180ea5add53979c58019055eed0e6
Sciroccogti <scirocco_gti@yeah.net>
2022-07-29 14:54:26 +0800

    better LDPC decoder script

--- experiments/python/LDPC_decoder_NET_main.py
+++ experiments/python/LDPC_decoder_NET_main.py
@@ -1,22 +1,26 @@
 import numpy as np
-import math_util
 import os
 import matmul as mm
 import math_util as mu
 import scipy.io as io
+from amm_methods import *
 
-snr = "14db/"
+snrs_ = ["8db/", "8.5db/", "9db/", "9.5db/", "10db/", "10.5db/", "11db/", "12db/", "14db/"]
 
-est = mm.estFactory(X_path="x.npy", W_path="w.npy", Y_path="y.npy", dir= os.path.join("ldpc", snr))
+method = METHOD_MITHRAL
 
-x_test = np.load("LDPC_decoder_NET_testdata/" + snr + "hidden3_output_test%sB.npy" % (snr[:-3]))
-w_test = np.load("LDPC_decoder_NET_testdata/" + snr + "weight.npy", allow_pickle=True)
-bias = np.load("LDPC_decoder_NET_testdata/" + snr + "bias.npy")
+for snr in snrs_:
+    print(snr)
+    est = mm.estFactory(X_path="x.npy", W_path="w.npy", Y_path="y.npy", dir= os.path.join("ldpc", snr), methods=[method])
 
-y_out_matmul = mm.eval_matmul(est, x_test, w_test) # MADDNESS乘法的结果
-y_out_last = mu.softmax(y_out_matmul + bias.T) # MADDNESS替换后当前层输出，即+bias并激活函数后的结果
+    x_test = np.load("data/" + "hidden3_output_test%sB.npy" % (snr[:-3]))
+    w_test = np.load("data/" + "weight.npy", allow_pickle=True)
+    bias = np.load("data/" + "bias.npy")
 
-np.save("LDPC_decoder_NET_testdata/" + snr + "nomul_matmul_yout_matmul", y_out_matmul)
-np.save("LDPC_decoder_NET_testdata/" + snr + "nomul_matmul_yout_last", y_out_last)
+    y_out_matmul = mm.eval_matmul(est, x_test, w_test) # MADDNESS乘法的结果
+    y_out_last = mu.softmax(y_out_matmul + bias.T) # MADDNESS替换后当前层输出，即+bias并激活函数后的结果
 
-io.savemat("LDPC_decoder_NET_testdata/" + snr + "NN_output_threshold0.3_Tr15_SNR%s_ot.mat" % (snr[:-3]), {"NN_output_buffer": y_out_last})
+    # np.save("LDPC_decoder_NET_testdata/" + snr + "nomul_matmul_yout_matmul", y_out_matmul)
+    # np.save("LDPC_decoder_NET_testdata/" + snr + "nomul_matmul_yout_last", y_out_last)
+
+    io.savemat("data/" + method + "_output_threshold0.3_Tr15_SNR%s_ot.mat" % (snr[:-3]), {"NN_output_buffer": y_out_last})
