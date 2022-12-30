9179fff334c4b454d5515fd9387ff4df40b278bd
Sciroccogti <scirocco_gti@yeah.net>
2022-12-30 22:55:04 +0800

    change encode_algo to normal

--- experiments/python/dpq/dpq_encoder.py
+++ experiments/python/dpq/dpq_encoder.py
@@ -3,7 +3,7 @@
 @author Sciroccogti (scirocco_gti@yeah.net)
 @brief 
 @date 2022-12-29 13:27:52
-@modified: 2022-12-30 21:48:08
+@modified: 2022-12-30 22:52:41
 '''
 
 import clusterize
@@ -43,10 +43,12 @@ class DPQEncoder(vq.MultiCodebookEncoder):
 
         # PQ style
         self.subvect_len = int(np.ceil(X.shape[1] / self.ncodebooks))
-        self.encode_algo = "multisplits"
-        self.splits_lists, self.centroids = clusterize.learn_splits_in_subspaces(
-            X, subvect_len=self.subvect_len, nsplits_per_subs=self.code_bits,
-            algo=self.encode_algo)
+        X = vq.ensure_num_cols_multiple_of(X, self.ncodebooks)
+        # self.encode_algo = "multisplits"
+        # self.splits_lists, self.centroids = clusterize.learn_splits_in_subspaces(
+        #     X, subvect_len=self.subvect_len, nsplits_per_subs=self.code_bits,
+        #     algo=self.encode_algo)
+        self.centroids = vq._learn_centroids(X, self.ncentroids, self.ncodebooks, self.subvect_len)
 
     def encode_Q(self, Q):
         '''
@@ -67,9 +69,15 @@ class DPQEncoder(vq.MultiCodebookEncoder):
         return luts
 
     def encode_X(self, X):
+        '''
+        encode left matrix online
+        :param X: online left matrix
+        '''
         # PQ style
         X = vq.ensure_num_cols_multiple_of(X, self.ncodebooks)
-        idxs = clusterize.encode_using_splits(X, self.subvect_len, self.splits_lists, "multi")
+        # idxs = clusterize.encode_using_splits(X, self.subvect_len, self.splits_lists, "multi")
+        idxs = vq.pq._encode_X_pq(X, codebooks=self.centroids)
+
         # self.offsets is set in MultiCodebookEncoder.__init__
         return idxs + self.offsets
 
