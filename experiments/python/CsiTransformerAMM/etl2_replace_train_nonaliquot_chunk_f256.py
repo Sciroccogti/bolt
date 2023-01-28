390c262c48ad33726bb65bc8f1ed0418c9f22bb3
hdright <hdright@outlook.com>
2023-01-28 15:23:16 +0800

    etl2 update

--- experiments/python/CsiTransformerAMM/etl2_replace_train_nonaliquot_chunk_f256.py
+++ experiments/python/CsiTransformerAMM/etl2_replace_train_nonaliquot_chunk_f256.py
@@ -31,7 +31,7 @@ import socket # Obtain the current host name, which can be used to select differ
 # method = METHOD_MITHRALPQ
 # method = METHOD_EXACT
 # method = METHOD_SCALAR_QUANTIZE
-quantize_lut = False
+quantize_lut = True
 for method in [METHOD_MITHRAL]:
 
     linear_name = 'etl2'
@@ -43,21 +43,21 @@ for method in [METHOD_MITHRAL]:
 
     if auto_train_change_upcast == True:
         if method == METHOD_MITHRAL:
-            upcast_trained = 16
-            upcast_goal = -1
+            upcast_trained = -1
+            upcast_goal = 16
         else:
             upcast_trained = -1
             upcast_goal = 16
     else:
         if method == METHOD_MITHRAL:
-            upcast_goal = -1
+            upcast_goal = 16
         else:
             upcast_goal = 16
 
 
-    nbits_trained = 8
+    nbits_trained = 0
     
-    nbits_goal = 0
+    nbits_goal = 16
     if quantize_lut == False:
         nbits_goal = 0
     nbits = nbits_goal # 要运行的量化比特数
@@ -91,7 +91,9 @@ for method in [METHOD_MITHRAL]:
     i=0
     # 遍历每个cb、ct、n_train_sam组合
     for _, row_ref in cb_ct_ntr_combinations_unique.iterrows():
-
+        if i in range(1):
+            i+=1
+            # continue
         ncodebooks = int(row_ref['cb'])
         ncentroids = int(row_ref['ct'])
         train_sam_num = int(row_ref['n_train_sam'])
